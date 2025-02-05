6d7160622751286af50df049c0ecac33a2877d4a
T0nhao <antonio.andre@sou.inteli.edu.br>
2025-02-05 09:00:13 -0300

    Adicionando arquivos

--- peixe.js
+++ peixe.js
@@ -16,12 +16,14 @@ var game= new Phaser.Game(config);
 var peixão;
 var tubas;
 
+
 function preload(){
      this.load.image ('mar','assets/bg_azul-escuro.png');
      this.load.image ('logo','assets/logo-inteli_branco.png');
-     this.load.image ('peix','assets/baiacu.png')
-     this.load.image ('tubas','assets/tubarao.png')
-     this.load.image ('concha','assets/concha.png')
+     this.load.image ('peix','assets/baiacu.png');
+     this.load.image ('tubas','assets/tubarao.png');
+     this.load.image ('concha','assets/concha.png');
+  
                   }
 
 function create(){
@@ -31,10 +33,11 @@ function create(){
    
 
     peixão =this.add.image(400,300,'peix');
-    peixão.setFlip(true,false);
+    peixão.setFlip(false,false);
 
     tubas =this.add.image(400,300,'tubas');
     tubas.setFlip(true,false);
+
 
                  }
 
@@ -42,10 +45,11 @@ function update(){
     peixão.x= this.input.x;
     peixão.y= this.input.y;
 
-    tubas.x=this.input.y;
-    tubas.y=this.input.x;
+    tubas.x=this.input.y+200;
+    tubas.y=this.input.x+200;
+
+   
 }
 
-var resposta = "Eu já havia programado antes em c++, bem pouco, mas já tinha uma lógica. Quando vi que estudaria javascript eu pesquisei um pouco para ter uma base só que não tinha chegado nessa parte apresentada por essa atividade. O que eu tenho a dizer sobre essa atividade é que são muitos detalhes e que eu tive que ficar toda hora olhando para relembrar cada ponto e aspas que tinha que colocar e eu espero que daqui pra frente fique mais fácil de pegar isso tudo";
-console.log(resposta);
+
 
